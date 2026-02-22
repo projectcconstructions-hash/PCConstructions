d59f625e6dcaf358065abeabd79cecdd425ce9b3
vikraamm <vikram.ravi95@gmail.com>
2026-02-21 22:01:30 -0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen font-['Jost']">
+      <h1 className="text-2xl font-bold p-4">PC Constructions</h1>
+    </div>
+  );
+}
+
+export default App;
